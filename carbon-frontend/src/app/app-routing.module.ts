d3d1de84797aa3285a332dd585c705259071e858
Ryan Pereira <rayoz12@yahoo.com.au>
2021-06-14 11:17:23 +1000

    progress

--- carbon-frontend/src/app/app-routing.module.ts
+++ carbon-frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CatalogComponent } from './pages/catalog/catalog.component';
 import { DocsComponent } from './pages/docs/docs.component';
 import { SupportComponent } from './pages/support/support.component';
 import { Link1Component } from './pages/link1/link1.component';
+import { LandAreasComponent } from "./pages/land-areas/land-areas.component";
 
 const routes: Routes = [
 	{
@@ -25,6 +26,10 @@ const routes: Routes = [
 	{
 		path: 'link1',
 		component: Link1Component
+	},
+	{
+		path: 'land-areas',
+		component: LandAreasComponent
 	}
 ];
 
